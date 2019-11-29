6466017f698013bc0486d1696252807d84e57955
dcastrelo <castrelo@gmail.com>
2019-11-29 08:39:46 -0300

    endstops en MAX
    que no funciono como quise

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -724,7 +724,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 3, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -920,7 +920,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -28, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 28, 0, 0 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 15
@@ -1055,10 +1055,10 @@
 #define Y_BED_SIZE 208
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -17
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS 160
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 200
 
@@ -1301,7 +1301,7 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-//#define MANUAL_X_HOME_POS -17
+//#define MANUAL_X_HOME_POS 0
 //#define MANUAL_Y_HOME_POS 0
 //#define MANUAL_Z_HOME_POS 0
 
